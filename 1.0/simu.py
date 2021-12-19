bae7c5915714c3dbb6753099f5af25448a5a57e8
wzq <wenzq20@163.com>
2021-12-19 12:10:15 +0000

    well performance version

--- 1.0/simu.py
+++ 1.0/simu.py
@@ -2,16 +2,25 @@
 from TOSSIM import *
 import sys
 
-out = sys.stdout
+import os
 
-# Number of nodes in the simulated network is 3
-number_of_nodes = 4
+logDir = "log"
+if not os.path.exists(logDir):
+    os.makedirs(logDir)
+
+stream = {
+    "Boot": sys.stdout,
+    "Radio": sys.stdout,
+    "Led": sys.stdout,
+    "RadioSend": open(logDir + "/send.log", "w"),
+    "RadioRec": open(logDir + "/rec.log", "w"),
+    "Pkg": open(logDir + "/pkg.log", "w")
+}
+
 
 t = Tossim([])
-m = t.mac()
 r = t.radio()
 
-#Open a topology file and parse the data, where first linebreak is transmitter nodeID, second is receiver nodeID and third is dBm value
 f = open("topo.txt", "r")
 for line in f:
     s = line.split()
@@ -19,41 +28,17 @@ for line in f:
         print " ", s[0], " ", s[1], " ", s[2];
         r.add(int(s[0]), int(s[1]), float(s[2]))
 
-# The type of debug messages that will be printed out. [add, comment and uncomment as you need]
-t.addChannel("Init",out)
-t.addChannel("Boot", out);
-t.addChannel("Radio", out);
-t.addChannel("Role",out);
-t.addChannel("Led", out);
-t.addChannel("RadioSend", out);
-t.addChannel("RadioRec", out);
-t.addChannel("Pkg", out);
-#t.addChannel("Drop", sys.stdout);
-#t.addChannel("Fwd", sys.stdout);
-#t.addChannel("BASE", sys.stdout);
-#t.addChannel("DBG", sys.stdout);
-#t.addChannel("ERR", sys.stdout);
-#t.addChannel("FILE", sys.stdout);
+for key, value in stream.items():
+    t.addChannel(key, value);
 
-#Boot Nodes
 
 time_boot = 0*t.ticksPerSecond();
 
-print("Creating node 1...");
-node1 = t.getNode(1);
-#time1 = 0*t.ticksPerSecond();
-node1.bootAtTime(time_boot);
+for i in range(1, 4):
+    node = t.getNode(i)
+    print "Creating node ", i, "..."
+    node.bootAtTime(time_boot + i * 5)
 
-
-print("Creating node 2...");
-node2 = t.getNode(2);
-#time1 = 0*t.ticksPerSecond();
-node2.bootAtTime(time_boot + 10);
-
-print("Creating node 3...");
-node3 = t.getNode(3);
-#time1 = 0*t.ticksPerSecond();
-node3.bootAtTime(time_boot + 15);
 
 #Add noise to the medium channel
 noise = open("meyer-heavy.txt", "r")
@@ -61,10 +46,10 @@ for line in noise:
     str1 = line.strip()
     if str1:
         val = int(str1)
-        for i in range(number_of_nodes):
+        for i in range(1, 4):
             t.getNode(i).addNoiseTraceReading(val)
 
-for i in range(1,4):
+for i in range(1, 4):
     print "Creating noise model for ",i;
     t.getNode(i).createNoiseModel()
 
@@ -72,3 +57,7 @@ for i in range(1,4):
 # Simulation time is set to 9999
 for i in range(1099):
     t.runNextEvent()
+
+for _, out in stream.items():
+    if out != sys.stdout:
+        out.close()
